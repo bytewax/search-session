c5ff19b7731edf9f551f3c2c6d5dee6f21e04f4a
Laura Gutierrez Funderburk <lgutierrwr@gmail.com>
2024-04-29 14:07:57 -0700

    upgrade code

--- dataflow.py
+++ dataflow.py
@@ -1,14 +1,14 @@
 from datetime import datetime, timedelta, timezone
-
 from dataclasses import dataclass
 from typing import List
 
-from bytewax.connectors.stdio import StdOutput
-from bytewax.window import (
-    EventClockConfig,
-    SessionWindow,
-)
-
+from bytewax.connectors.stdio import StdOutSink
+from bytewax.operators.window import SessionWindow, \
+                                    EventClockConfig
+from bytewax.operators import window as wop
+import bytewax.operators as op
+from bytewax.dataflow import Dataflow
+from bytewax.testing import TestingSource, run_main
 
 @dataclass
 class AppOpen:
@@ -22,13 +22,11 @@ class Search:
     query: str
     time: datetime
 
-
 @dataclass
 class Results:
     user: int
     items: List[str]
     time: datetime
-
 
 @dataclass
 class ClickResult:
@@ -37,66 +35,71 @@ class ClickResult:
     time: datetime
 
 
-from bytewax.dataflow import Dataflow
-
-flow = Dataflow()
-
-from bytewax.testing import TestingInput
-
-
-# The time at which we want all of our windows to align to
-align_to = datetime(2022, 1, 1, tzinfo=timezone.utc)
-
-# Simulated events to emit into our Dataflow
-client_events = [
-    Search(user=1, time=align_to + timedelta(seconds=5), query="dogs"),
-    Results(1, time=align_to + timedelta(seconds=6), items=["fido", "rover", "buddy"]),
-    ClickResult(1, time=align_to + timedelta(seconds=7), item="rover"),
-    Search(2, time=align_to + timedelta(seconds=5), query="cats"),
-    Results(
-        2,
-        time=align_to + timedelta(seconds=6),
-        items=["fluffy", "burrito", "kathy"],
-    ),
-    ClickResult(2, time=align_to + timedelta(seconds=7), item="fluffy"),
-    ClickResult(2, time=align_to + timedelta(seconds=8), item="kathy"),
-]
-
-
-flow.input("input", TestingInput(client_events))
-# event
-
-
 def user_event(event):
+    
     return str(event.user), event
-
-
-flow.map(user_event)
-# (user, event)
-
 
 def add_event(acc, event):
     acc.append(event)
     return acc
 
-
-clock_config = EventClockConfig(
-    lambda e: e.time, wait_for_system_duration=timedelta(seconds=0)
-)
-window_config = SessionWindow(gap=timedelta(seconds=5))
-flow.fold_window("minute_windows", clock_config, window_config, list, add_event)
-# ('1', [Search(user=1, query='dogs', time=datetime.datetime...)])
-
-
 def calc_ctr(user__search_session):
-    user, search_session = user__search_session
+    user, (window_metadata, search_session) = user__search_session
+    
     searches = [event for event in search_session if isinstance(event, Search)]
     clicks = [event for event in search_session if isinstance(event, ClickResult)]
-    return (user, (len(clicks) / len(searches)))
+    
+    # See counts of searches and clicks
+    print(f"User {user}: {len(searches)} searches, {len(clicks)} clicks")
+
+    if len(searches) == 0:
+        return (user, 0)
+    return (user, len(clicks) / len(searches))
 
 
+# The time at which we want all of our windows to align to
+align_to = datetime(2024, 1, 1, tzinfo=timezone.utc)
+
+# Simulated events to emit into our Dataflow
+# In these events we have two users, each of which 
+# performs a search, gets results, and clicks on a result
+# User 1 searches for dogs, clicks on rover
+# User 2 searches for cats, clicks on fluffy and kathy
+client_events = [
+    Search(user=1, time=align_to + timedelta(seconds=5), query="dogs"),
+    Results(user=1, time=align_to + timedelta(seconds=6), items=["fido", "rover", "buddy"]),
+    ClickResult(user=1, time=align_to + timedelta(seconds=7), item="rover"),
+    Search(user=2, time=align_to + timedelta(seconds=5), query="cats"),
+    Results(
+        user=2,
+        time=align_to + timedelta(seconds=6),
+        items=["fluffy", "burrito", "kathy"],
+    ),
+    ClickResult(user=2, time=align_to + timedelta(seconds=7), item="fluffy"),
+    ClickResult(user=2, time=align_to + timedelta(seconds=8), item="kathy"),
+]
+
+# Configuration for the Dataflow 
+event_time_config = EventClockConfig(
+    dt_getter=lambda e: e.time,
+    wait_for_system_duration=timedelta(seconds=1)  
+)
+# Configuration for the windowing operator
+clock_config = SessionWindow(gap=timedelta(seconds=10))
+
+# Create the Dataflow
+flow = Dataflow("search_ctr")
+# Add input source
+inp = op.input("inp", flow, TestingSource(client_events))
+# Map user events function
+user_event_map = op.map("user_event", inp, user_event)
+# Collect windowed data
+window = wop.collect_window("windowed_data", \
+                            user_event_map, \
+                            clock=event_time_config,
+                            windower=clock_config)
 # Calculate search CTR.
-flow.map(calc_ctr)
-# ('1', 1.0)
-# ('2', 2.0)
-flow.output("stdout", StdOutput())
+calc = op.map("calc_ctr", window, calc_ctr)
+# Output the results
+op.output("out", calc, StdOutSink())
+
